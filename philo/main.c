e7119997dcaae8addb239ef55916ffd5db21d5c4
edoardo <edoardovocaturo@gmail.com>
2023-08-10 17:55:36 +0200

    .

--- philo/main.c
+++ philo/main.c
@@ -3,17 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: evocatur <evocatur@student.42.fr>          +#+  +:+       +#+        */
+/*   By: edoardo <edoardo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 12:17:57 by evocatur          #+#    #+#             */
-/*   Updated: 2023/08/09 12:50:51 by evocatur         ###   ########.fr       */
+/*   Updated: 2023/08/10 17:55:30 by edoardo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
-void *myThreadFun(void *vargp);
 
-  
+void *myThreadFun(void *vargp)
+{
+    
+    
+}
+
 int main(int argc, char **argv)
 {
     int             i;
@@ -23,5 +27,15 @@ int main(int argc, char **argv)
     if (argc < 5)
         exit(0);
     check(argv);
+    p.number_of_philosophers = argv[1];
+    p.time_to_die = argv[2];
+    p.time_to_eat = argv[3];
+    p.time_to_sleep = argv[4];
+    p.number_of_times_each_philosopher_must_eat = argv[5];
+    for (i = 0; i <p.number_of_philosophers; i++)
+    {
+        pthread_create(&tid, NULL, myThreadFun, (void *)&tid);
+    }
+    pthread_exit(NULL);
     return 0;
 }
