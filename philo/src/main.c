c7f9595cb2f3eb1905f96b34cc48115acc61bb08
edoardo vocaturo <edoardovocaturo@gmail.com>
2023-09-06 11:23:03 +0000

    test

--- philo/src/main.c
+++ philo/src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: evocatur <evocatur@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fborroto <fborroto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 12:17:57 by evocatur          #+#    #+#             */
-/*   Updated: 2023/08/24 16:27:09 by evocatur         ###   ########.fr       */
+/*   Updated: 2023/09/06 13:18:13 by fborroto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,39 @@
 
 void *myThreadFun(void *vargp)
 {
-    printf("asdasdasd\n");
-    return 0;
+    int *ts = (int *)malloc(sizeof(int));
+    *ts = 5;
+    return ts;
 }
 
 int main(int argc, char **argv)
 {
     int             i;
+    int             *result;
     t_philosophers  p;
-    pthread_t       tid;
+    pthread_t       newthread;
 
+    i = 0;
+    
     if (argc < 5)
         exit(0);
+
+
     p.number_of_philosophers = atoi(argv[1]);
     p.time_to_die = atoi(argv[2]);
     p.time_to_eat = atoi(argv[3]);
     p.time_to_sleep = atoi(argv[4]);
-    p.number_of_times_each_philosopher_must_eat = atoi(argv[5]);
-    for (i = 0; i < p.number_of_philosophers; i++)
+    p.each_philo_must_eat = atoi(argv[5]);
+    
+    while (i < p.number_of_philosophers)
     {
-        pthread_create(&tid, NULL, myThreadFun, (void *)&tid);
+        pthread_create(&newthread, NULL, myThreadFun, NULL);        
+        i++;
     }
-    pthread_exit(NULL);
+    
+
+
+    pthread_join(newthread, (void *)&result);
+    printf("%i\n",*result);
     return 0;
 }
--- philo/src/philosophers.h
+++ philo/src/philosophers.h
@@ -3,24 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: evocatur <evocatur@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fborroto <fborroto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 12:15:46 by evocatur          #+#    #+#             */
-/*   Updated: 2023/08/24 16:25:44 by evocatur         ###   ########.fr       */
+/*   Updated: 2023/09/06 13:10:26 by fborroto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h> 
-#include <sys/time.h>
 
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
-
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h> 
+# include <sys/time.h>
 
 typedef struct s_philosophers
 {
@@ -28,7 +26,7 @@ typedef struct s_philosophers
     int  time_to_die;
     int  time_to_eat;
     int  time_to_sleep;
-    int  number_of_times_each_philosopher_must_eat;
+    int  each_philo_must_eat;
 }   t_philosophers;
 
 void check(char **argv);
